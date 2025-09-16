eaf70a8fb15a2768e71df9df1c983a35c9e6c78b
IT24102394 <it24102394@my.sliit.lk>
2025-09-16 23:17:10 +0530

    IT24102394_Lab07

---
+++ Lab Sheet 07/IT24102394.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\User\\Desktop\\IT24102394")
+
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3 (i)
+1-pnorm(130, mean=100, sd=15)
+
+#3 (ii)
+qnorm(0.95, mean=100, sd=15)
